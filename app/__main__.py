f9c8c9c7fcf56052516ba4f4418f789b9a84ced1
Known_Black_Hat <knownblackhat@gmail.com>
2024-05-17 17:26:49 -0400

    chore: add base code to run

--- app/__main__.py
+++ app/__main__.py
@@ -17,3 +17,19 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 app = FastAPI(openapi_url="", redoc_url=None)
 app.include_router(root)
 app.include_router(suggestion_endpoint)
+
+from endpoints import root, suggestion
+from fastapi import FastAPI
+
+app = FastAPI()
+
+app.include_router(root.router)
+app.include_router(suggestion.router)
+
+if __name__ == "__main__":
+    import os
+
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
